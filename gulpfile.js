62e5db3cff98a5f93e4fd2958a257b61ffd3718b
Guilherme Diego <guidiego.expgames@gmail.com>
2018-01-09 17:47:26 -0200

    Doing: refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,38 @@
 'use strict';
 
 var gulp = require( 'gulp' );
+var year = require('optimist').argv.year;
+var eventFile = './events/' + year + '.json';
 var connect = require( 'gulp-connect' );
+var handlebars = require( 'gulp-compile-handlebars' );
+var rename = require( 'gulp-rename' );
 var files = [ 'index.html', '../assets/css/style.css' ];
+var handlebarsFiles = [ './views/**/*', eventFile ];
+var _ = require('lodash');
+
+var options = {
+  batch: ['./views/partials' ],
+};
+
+
+function getEventData() {
+  var eventData = require(eventFile);
+  eventData.speakers = _.chain(eventData.schedule)
+                        .map(function getSpeakers(event) { return event.speaker; })
+                        .compact()
+                        .sortBy('priority')
+                        .value();
+
+  return eventData;
+}
+
+
+gulp.task( 'hbs', function () {
+  return gulp.src('views/index.hbs')
+            .pipe(handlebars(getEventData(), options))
+            .pipe(rename('index.html'))
+            .pipe(gulp.dest('./'));
+});
 
 gulp.task( 'files', function() {
   gulp.src( files ).pipe( connect.reload() );
@@ -10,10 +40,11 @@ gulp.task( 'files', function() {
 
 gulp.task( 'watch', function() {
   gulp.watch( files, [ 'files' ]);
+  gulp.watch( handlebarsFiles, [ 'hbs' ]);
 });
 
 gulp.task( 'connect', function() {
   connect.server({ livereload: true });
 });
 
-gulp.task('default', [ 'connect', 'watch' ]);
+gulp.task('default', [ 'hbs', 'connect', 'watch' ]);
